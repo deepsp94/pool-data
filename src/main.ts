2ccf12b68cc569b997441154b52305783b048691
deepsp94 <anantsp94@gmail.com>
2024-05-01 13:44:17 -0400

    Save to csv and load from dot env

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,24 @@ import { normalizeAssetInfo } from "@/helpers";
 import { getBlockTimestampByHeight, initLCD, queryRawContract, querySmartContract } from "@/libs/lcd";
 import { num } from "@/libs/num";
 import { PoolInfo, PCLPoolRawConfig } from './types';
+import { writeFileSync } from 'fs';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 async function main() {
-  initLCD("https://rest-kralum.neutron-1.neutron.org/");
+  // Check if the LCD_URL is provided in the .env file
+  const lcdUrl = process.env.LCD_URL;
+  if (!lcdUrl) {
+    console.error("LCD_URL is not set in the environment variables. Please set it in the .env file.");
+    return; // Exit the function if LCD_URL is not set
+  }
+
+  initLCD(lcdUrl);
+
 
   const POOL_ADDRESS = "neutron1j4xpv03fw664mvntlhqnzp5hjqk2nfw00vrgx9qlq97rxc9fu3lqvmszl2";
-  const START_BLOCK_HEIGHT = 9482351;
-  const END_BLOCK_HEIGHT = 9482351 + 2;
+  const START_BLOCK_HEIGHT = 9295300;
+  const END_BLOCK_HEIGHT = 9700000;
 
   const data: any[] = [];
   for (let blockHeight = START_BLOCK_HEIGHT; blockHeight <= END_BLOCK_HEIGHT; blockHeight += 100) {
@@ -43,12 +54,12 @@ async function main() {
         normalizeAssetInfo(asset1.info),
         normalizeAssetInfo(asset2.info),
         "total_lp",
-        "initial_amp",
-        "initial_gamma",
-        "initial_time",
-        "future_amp",
-        "future_gamma",
-        "future_time",
+        // "initial_amp",
+        // "initial_gamma",
+        // "initial_time",
+        // "future_amp",
+        // "future_gamma",
+        // "future_time",
         "price_state:oracle_price",
         "price_state:last_price",
         "price_state:price_scale",
@@ -69,12 +80,12 @@ async function main() {
         asset1.amount,
         asset2.amount,
         totalLp,
-        rawConfig.pool_state.initial.amp,
-        rawConfig.pool_state.initial.gamma,
-        rawConfig.pool_state.initial_time,
-        rawConfig.pool_state.future.amp,
-        rawConfig.pool_state.future.gamma,
-        rawConfig.pool_state.future_time,
+        // rawConfig.pool_state.initial.amp,
+        // rawConfig.pool_state.initial.gamma,
+        // rawConfig.pool_state.initial_time,
+        // rawConfig.pool_state.future.amp,
+        // rawConfig.pool_state.future.gamma,
+        // rawConfig.pool_state.future_time,
         rawConfig.pool_state.price_state.oracle_price,
         rawConfig.pool_state.price_state.last_price,
         rawConfig.pool_state.price_state.price_scale,
@@ -84,7 +95,15 @@ async function main() {
       ];
       console.log(row.join(","));
       data.push(row);
+
+      await sleep(10); // Milliseconds
     }
+
+    const csvContent = data.map(row => row.join(",")).join("\n");
+
+    writeFileSync("price_data.csv", csvContent);
+
+    console.log("Data saved to price_data.csv");
   }
 
   // console.log("Data", JSON.stringify(data, null, 2));
